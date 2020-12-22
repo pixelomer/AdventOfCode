bdee4f81326f6c51d11d6ebf71eba571e1a7dac4
pixelomer <37103802+pixelomer@users.noreply.github.com>
2020-12-22 11:13:04 +0300

    Removed logging code from year 2020 day 22

--- 2020/Day 22 - Crab Combat/solution.js
+++ 2020/Day 22 - Crab Combat/solution.js
@@ -1,30 +1,19 @@
 module.exports = (input, part) => {
-	console.log = ()=>{}; // comment to make verbose
-
-	function playCombat(players, recursive = false, ID = 1) {
-		console.log(`=== Game ${ID} ===\n`);
-		let previousStates = {};
-		let round = 1;
+	function playCombat(players, recursive = false) {
+		let previousStates = new Set();
 		players = [...players];
 		let winningPlayer;
 		while (!Object.values(players).some((array) => (array.length == 0))) {
 			if (recursive) {
 				const key = `${players[0].join(",")}|${players[1].join(",")}`;
-				if (previousStates[key]) {
+				if (previousStates.has(key)) {
 					winningPlayer = 0;
 					break;
 				}
-				previousStates[key] = true;
+				previousStates.add(key);
 			}
 			winningPlayer = 1;
-			console.log(
-`-- Round ${round} (Game ${ID}) --
-Player 1's deck: ${players[0].join(", ")}
-Player 2's deck: ${players[1].join(", ")}`);
 			let newCards = [players[0].shift(), players[1].shift()];
-			console.log(
-`Player 1 plays: ${newCards[0]}
-Player 2 plays: ${newCards[1]}`);
 			if (
 				recursive
 				&& (newCards[0] <= players[0].length)
@@ -34,9 +23,7 @@ Player 2 plays: ${newCards[1]}`);
 					players[0].slice(0, newCards[0]),
 					players[1].slice(0, newCards[1])
 				];
-				console.log("Playing a sub-game to determine the winner...\n");
-				winningPlayer = playCombat(newPlayers, true, ID+1).winner;
-				console.log("...anyway, back to game ");
+				winningPlayer = playCombat(newPlayers, true).winner;
 			}
 			else {
 				if (newCards[0] > newCards[1]) {
@@ -44,10 +31,8 @@ Player 2 plays: ${newCards[1]}`);
 				}
 			}
 			newCards = [newCards[winningPlayer], newCards[(winningPlayer == 1) ? 0 : 1]];
-			console.log(`Player ${winningPlayer+1} wins round ${round++} of game ${ID}!\n`);
 			players[winningPlayer].push(...newCards);
 		}
-		console.log(`The winner of game ${ID} is ${winningPlayer}!\n`);
 		return {
 			winner: winningPlayer,
 			winnerScore: players[winningPlayer].reduce((acc, card, index) =>
