c17a63224584892f969b659ce1d01934592a2522
pixelomer <37103802+pixelomer@users.noreply.github.com>
2020-12-25 08:40:39 +0300

    Added solution for year 2020 day 25 part 1
    Not sure if there's a part 2 yet, I need to solve day 23 first

---
+++ 2020/Day 25 - Combo Breaker/solution.js
@@ -0,0 +1,36 @@
+module.exports = (input, part) => {
+	if (part === 2) {
+		return null;
+	}
+	
+	const split = input.split("\n");
+	const doorPublicKey = parseInt(split[0]);
+	const cardPublicKey = parseInt(split[1]);
+
+	function transformOnce(value, subjectNumber) {
+		return (value * subjectNumber) % 20201227;
+	}
+
+	function transform(subjectNumber, loopSize) {
+		let value = 1;
+		for (let i=0; i<loopSize; i++) {
+			value = transformOnce(value, subjectNumber);
+		}
+		return value;
+	}
+
+	function findLoopSize(publicKey) {
+		let loopSize = 1;
+		let value = 1;
+		while (true) {
+			value = transformOnce(value, 7);
+			if (value == publicKey) return loopSize;
+			loopSize++;
+		}
+	}
+
+	const cardLoopSize = findLoopSize(cardPublicKey);
+	const doorLoopSize = findLoopSize(doorPublicKey);
+
+	return transform(cardPublicKey, doorLoopSize);
+};
