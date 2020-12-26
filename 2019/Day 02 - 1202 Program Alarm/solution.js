9f88b46dc204d5b2b375be90861c198c1beca043
pixelomer <37103802+pixelomer@users.noreply.github.com>
2020-12-26 20:57:17 +0300

    Added solution for year 2019 day 2

--- 2019/Day 02 - 1202 Program Alarm/solution.js
+++ 2019/Day 02 - 1202 Program Alarm/solution.js
@@ -1,3 +1,57 @@
-module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+module.exports = (input, part, isTest) => {
+	const program = input.split(",").map((val) => parseInt(val));
+
+	function run(memory) {
+		memory = [...memory]; // Create a copy
+		let PC = 0;
+		while (PC < memory.length) {
+			//console.log(memory);
+			const instruction = memory[PC++];
+			let x, y;
+			switch (instruction) {
+				case 1:
+					// *p2 = (*p0) + (*p1)
+					x = memory[memory[PC++]];
+					y = memory[memory[PC++]];
+					memory[memory[PC++]] = x + y;
+					break;
+				case 2:
+					// *p2 = (*p0) * (*p1)
+					x = memory[memory[PC++]];
+					y = memory[memory[PC++]];
+					memory[memory[PC++]] = x * y;
+					break;
+				case 99:
+					// halt
+					PC = memory.length;
+					break;
+				default:
+					// illegal instruction
+					PC = memory.length;
+					console.log(`Illegal instruction: ${instruction}`);
+					break;
+			}
+		}
+		return memory;
+	}
+
+	if (part === 1) {
+		if (!isTest) {
+			program[1] = 12;
+			program[2] = 2;
+		}
+		return run(program)[0];
+	}
+	else {
+		for (let noun = 0; noun <= 99; noun++) {
+			for (let verb = 0; verb <= 99; verb++) {
+				const newProgram = [...program];
+				newProgram[1] = noun;
+				newProgram[2] = verb;
+				if (run(newProgram)[0] === 19690720) {
+					return 100 * noun + verb;
+				}
+			}
+		}
+	}
 };
