7990fa9beaaf8d556ae35c0da208dfb690202d3c
pixelomer <37103802+pixelomer@users.noreply.github.com>
2020-12-28 18:43:31 +0300

    Added solution for year 2019 day 11

--- 2019/Day 11 - Space Police/solution.js
+++ 2019/Day 11 - Space Police/solution.js
@@ -1,3 +1,53 @@
+const intcode = require("../intcode");
+
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	const program = input.split(",").map((val) => parseInt(val));
+	const machine = intcode.run(program);
+	const map = new Map();
+	if (part === 2) map.set("0,0", 1);
+	const offsets = [
+		{ dx: 0, dy: -1 },
+		{ dx: -1, dy: 0 },
+		{ dx: 0, dy: +1 },
+		{ dx: +1, dy: 0 }
+	];
+	let direction = 0;
+	let x = 0;
+	let y = 0;
+	let minX, maxX, minY, maxY;
+	minX = minY = Number.MAX_SAFE_INTEGER;
+	maxX = maxY = Number.MIN_SAFE_INTEGER;
+	while (machine.state == intcode.State.SUSPENDED) {
+		let key = `${x},${y}`;
+		machine.input = [map.get(key) ?? 0];
+		intcode.continueExecution(machine);
+		const newColor = machine.output.pop();
+		const directionChange = machine.output.pop();
+		if (directionChange == 1) {
+			direction = (direction + 3) % 4;
+		}
+		else {
+			direction = (direction + 1) % 4
+		}
+		map.set(key, newColor);
+		x += offsets[direction].dx;
+		y += offsets[direction].dy;
+		minX = Math.min(minX, x);
+		minY = Math.min(minY, y);
+		maxX = Math.max(maxX, x);
+		maxY = Math.max(maxY, y);
+	}
+	if (part === 1) {
+		return map.size;
+	}
+	else {
+		let result = "";
+		for (let y=minY; y<=maxY; y++) {
+			for (let x=minX; x<=maxX; x++) {
+				result += map.get(`${x},${y}`) ? "#" : " ";
+			}
+			result += "\n";
+		}
+		return result.substr(0, result.length-1);
+	}
 };
