eddcead538711c7e7ed436b58693bad51f58b157
pixelomer <37103802+pixelomer@users.noreply.github.com>
2020-12-28 11:14:38 +0300

    Added solution for year 2019 day 5

--- 2019/Day 05 - Sunny with a Chance of Asteroids/solution.js
+++ 2019/Day 05 - Sunny with a Chance of Asteroids/solution.js
@@ -1,3 +1,11 @@
+const intcode = require("../intcode");
+
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	const program = input.split(",").map((val) => parseInt(val));
+	const machine = intcode.run(program, [(part === 2) ? 5 : 1]);
+	if ((machine.unusedInput.length != 0) || (machine.output.slice(1).some((val) => val))) {
+		console.log("[-] Machine tests failed");
+		return null;
+	}
+	return machine.output[0];
 };
--- 2019/intcode.js
+++ 2019/intcode.js
@@ -4,42 +4,137 @@ const State = {
 	ILLEGAL_INSTRUCTION: 2
 };
 
-Object.keys(State).forEach((key) => {
-	State[State[key]] = key;
-});
+const ParameterMode = {
+	POSITION: 0,
+	IMMEDIATE: 1
+}
 
-function run(memory) {
+function constructEnum(obj) {
+	Object.keys(obj).forEach((key) => {
+		obj[obj[key]] = key;
+	});
+}
+
+constructEnum(ParameterMode);
+constructEnum(State);
+
+function run(memory, input = []) {
 	memory = [...memory];
+	input = [...input];
+	const output = [];
 	const machine = {
 		PC: 0,
 		state: State.RUNNING,
-		memory: memory
+		memory: memory,
+		unusedInput: input,
+		output: output
 	};
+
+	function illegalInstruction() {
+		throw new Error();
+	}
+
+	function read(address, mode = ParameterMode.POSITION) {
+		switch (mode) {
+			case ParameterMode.IMMEDIATE:
+				return address;
+			case ParameterMode.POSITION:
+				if (address > memory.length) {
+					illegalInstruction();
+				}
+				return memory[address];
+			default:
+				illegalInstruction();
+		}
+	}
+
+	function write(address, value) {
+		if (address > memory.length) {
+			illegalInstruction();
+		}
+		memory[address] = value;
+	}
+
+	function getInput() {
+		if (input.length == 0) {
+			illegalInstruction();
+		}
+		return input.shift();
+	}
+
 	while (machine.PC < memory.length) {
 		//console.log(memory);
-		const instruction = memory[machine.PC++];
-		let x, y;
-		switch (instruction) {
-			case 1:
-				// *p2 = (*p0) + (*p1)
-				x = memory[memory[machine.PC++]];
-				y = memory[memory[machine.PC++]];
-				memory[memory[machine.PC++]] = x + y;
-				break;
-			case 2:
-				// *p2 = (*p0) * (*p1)
-				x = memory[memory[machine.PC++]];
-				y = memory[memory[machine.PC++]];
-				memory[memory[machine.PC++]] = x * y;
-				break;
-			case 99:
-				// halt
-				machine.state = State.HALTED;
-				break;
-			default:
-				// illegal instruction
-				machine.state = State.ILLEGAL_INSTRUCTION;
-				break;
+		const initialPC = machine.PC;
+		try {
+			let instruction = read(machine.PC++);
+			const mode = [
+				ParameterMode.POSITION,
+				ParameterMode.POSITION,
+				ParameterMode.POSITION
+			];
+			for (let i=0, val=Math.floor(instruction / 100); val != 0; val = Math.floor(val / 10), i++) {
+				mode[i] = val % 10;
+			}
+			let x, y;
+			instruction %= 100;
+			switch (instruction) {
+				case 1:
+					// *p2 = (*)p0 + (*)p1
+					x = read(read(machine.PC++), mode[0]);
+					y = read(read(machine.PC++), mode[1]);
+					write(read(machine.PC++), x + y);
+					break;
+				case 2:
+					// *p2 = (*)p0 * (*)p1
+					x = read(read(machine.PC++), mode[0]);
+					y = read(read(machine.PC++), mode[1]);
+					write(read(machine.PC++), x * y);
+					break;
+				case 99:
+					// halt
+					machine.state = State.HALTED;
+					break;
+				case 3:
+					// *p0 = get_input()
+					write(read(machine.PC++), getInput());
+					break;
+				case 4:
+					// output >>= 1
+					// output[0] = (*)p0
+					output.unshift(read(read(machine.PC++), mode[0]));
+					break;
+				case 5:
+					// if ((*)p0) PC = (*)p1
+					x = read(read(machine.PC++), mode[0]);
+					y = read(read(machine.PC++), mode[1]);
+					if (x) machine.PC = y;
+					break;
+				case 6:
+					// if (!(*)p0) PC = (*)p1
+					x = read(read(machine.PC++), mode[0]);
+					y = read(read(machine.PC++), mode[1]);
+					if (!x) machine.PC = y;
+					break;
+				case 7:
+					// *p2 = (*)p0 < (*)p1
+					x = read(read(machine.PC++), mode[0]);
+					y = read(read(machine.PC++), mode[1]);
+					write(read(machine.PC++), (x < y) ? 1 : 0);
+					break;
+				case 8:
+					// *p2 = (*)p0 == (*)p1
+					x = read(read(machine.PC++), mode[0]);
+					y = read(read(machine.PC++), mode[1]);
+					write(read(machine.PC++), (x == y) ? 1 : 0);
+					break;
+				default:
+					throw new Error();
+			}
+		}
+		catch {
+			// Rollback
+			machine.state = State.ILLEGAL_INSTRUCTION;
+			machine.PC = initialPC;
 		}
 		if (machine.state != State.RUNNING) {
 			break;
