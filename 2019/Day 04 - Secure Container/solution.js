8581cc9e0f1c9627daaf24317b3c7c86bca5ed34
pixelomer <37103802+pixelomer@users.noreply.github.com>
2020-12-28 09:07:25 +0300

    Added solution for year 2019 day 4
    test1.txt was broken apparently

--- 2019/Day 04 - Secure Container/solution.js
+++ 2019/Day 04 - Secure Container/solution.js
@@ -1,3 +1,31 @@
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	const split = input.split("-").map(val => parseInt(val));
+	let start = Math.max(split[0], 100000);
+	let end = Math.min(split[1], 999999);
+	if (end < start) {
+		[end, start] = [start, end];
+	}
+	let count = 0;
+	for (let i=start; i<=end; i++) {
+		const str = i.toString();
+		const matches = str.match(/([0-9])\1+/g);
+		if (matches == null) {
+			continue;
+		}
+		if ((part === 2) && !matches.some((val) => val.length == 2)) {
+			continue;
+		}
+		const array = str.split("").map(val => parseInt(val));
+		let max = -1;
+		let j;
+		for (j=0; j<6; j++) {
+			if (max > array[j]) break;
+			max = array[j];
+		}
+		if (j != 6) {
+			continue;
+		}
+		count++;
+	}
+	return count;
 };
