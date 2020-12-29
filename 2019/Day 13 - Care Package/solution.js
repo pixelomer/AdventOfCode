f104969b24bed2e4b37d6db16abec0b53101f04b
pixelomer <37103802+pixelomer@users.noreply.github.com>
2020-12-29 11:09:35 +0300

    Added solution for year 2019 day 13 part 1
    Part 2 doesn't work and I don't know why. I feel like my intcode interpreter has some issues.

--- 2019/Day 13 - Care Package/solution.js
+++ 2019/Day 13 - Care Package/solution.js
@@ -1,3 +1,68 @@
+const intcode = require("../intcode");
+
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	if (part === 2) return;
+
+	const program = input.split(",").map((val) => parseInt(val));
+	program[0] = 2;
+	const machine = intcode.run(program);
+	const screen = new Map();
+	let paddleX;
+	let ballX;
+	let score;
+	let minX, minY, maxX, maxY;
+	minY = minX = Number.MAX_SAFE_INTEGER;
+	maxX = maxY = Number.MIN_SAFE_INTEGER;
+	function handleNewOutput() {
+		const x = machine.output.pop();
+		const y = machine.output.pop();
+		maxY = Math.max(maxY, y);
+		maxX = Math.max(maxX, x);
+		minY = Math.min(minY, y);
+		minX = Math.min(minX, x);
+		const tileID = machine.output.pop();
+		if ((x === -1) && (y === 0)) {
+			score = tileID;
+		}
+		else if (tileID === 3) {
+			paddleX = x;
+		}
+		else if (tileID === 4) {
+			ballX = x;
+		}
+		screen.set(`${x},${y}`, tileID);
+	}
+	
+	while (machine.output.length) {
+		handleNewOutput();
+	}
+	if (part === 1) {
+		const iterator = screen.values();
+		let tileID = iterator.next();
+		let blockTileCount = 0;
+		while (!tileID.done) {
+			if (tileID.value == 2) {
+				blockTileCount++;
+			}
+			tileID = iterator.next();
+		}
+		return blockTileCount;
+	}
+	else {
+		// This code doesn't work
+		/*while (machine.state !== intcode.State.HALTED) {
+			if (paddleX < ballX) {
+				machine.input = [+1];
+			}
+			else if (paddleX > ballX) {
+				machine.input = [-1];
+			}
+			else {
+				machine.input = [+1];
+			}
+			intcode.continueExecution(machine);
+			handleNewOutput();
+		}
+		console.log("Score:", score);*/
+	}
 };
