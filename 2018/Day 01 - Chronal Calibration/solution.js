06666cc7d0c87a9768a021f898706cb04d214de1
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-01-01 20:47:17 +0300

    Added solution for year 2018 day 1

--- 2018/Day 01 - Chronal Calibration/solution.js
+++ 2018/Day 01 - Chronal Calibration/solution.js
@@ -1,3 +1,19 @@
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	input = input.split("\n").map((val) => parseInt(val));
+	if (part === 1) {
+		return input.reduce(((acc, val) => acc + val), 0);
+	}
+	else {
+		const set = new Set([0]);
+		let frequency = 0;
+		while (true) {
+			if (input.some((val) => {
+				frequency += val;
+				if (set.has(frequency)) return true;
+				set.add(frequency);
+			})) {
+				return frequency;
+			}
+		}
+	}
 };
