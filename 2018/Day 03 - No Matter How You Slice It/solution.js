da4f88447a1e6a7be4951f03d8828e57dce70d91
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-11-28 13:42:32 +0300

    Added solution for year 2018 day 3

--- 2018/Day 03 - No Matter How You Slice It/solution.js
+++ 2018/Day 03 - No Matter How You Slice It/solution.js
@@ -1,3 +1,39 @@
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	const area = {};
+	const claims = input.split('\n').map((val) => {
+		const parsed = val.match(/#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)/);
+		return {
+			id: +parsed[1],
+			x: +parsed[2],
+			y: +parsed[3],
+			w: +parsed[4],
+			h: +parsed[5]
+		};
+	});
+	for (const {w, h, x, y} of claims) {
+		for (let i=0; i<w; i++) {
+			for (let j=0; j<h; j++) {
+				const key = `${x+i},${y+j}`;
+				area[key] = area[key] == null ? 0 : 1;
+			}
+		}
+	}
+	if (part === 2) {
+		for (const {id, w, h, x, y} of claims) {
+			let yes = true;
+			for (let i=0; i<w; i++) {
+				for (let j=0; j<h; j++) {
+					const key = `${x+i},${y+j}`;
+					if (area[key] === 1) {
+						yes = false;
+					}
+				}
+			}
+			if (yes === true) {
+				return id;
+			}
+		}
+		return null;
+	}
+	return Object.values(area).reduce((prev, curr) => prev + curr, 0);
 };
