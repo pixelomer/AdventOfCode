aaf6d946e34c05ce532ef2ee72fdbbc2f361250a
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-12-02 21:23:52 +0300

    Added solution for year 2016 day 3

--- 2016/Day 03 - Squares With Three Sides/solution.js
+++ 2016/Day 03 - Squares With Three Sides/solution.js
@@ -1,3 +1,20 @@
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	let specs;
+	if (part === 1) {
+		specs = input.split("\n").map((a) => a.match(/[0-9]+/g).map((a) => +a));
+	}
+	else {
+		const numbers = input.match(/[0-9]+/g).map((a) => +a);
+		specs = [];
+		for (let i=0; i<numbers.length; i+=9) {
+			for (let j=0; j<3; j++) {
+				specs.push([numbers[i+j], numbers[i+j+3], numbers[i+j+6]]);
+			}
+		}
+	}
+	return specs.filter((a) => (
+		(a[0] + a[1] > a[2]) &&
+		(a[2] + a[1] > a[0]) &&
+		(a[0] + a[2] > a[1])
+	)).length;
 };
