68181bfba7ce0d2a5431adfe2f76a7baae8721f9
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-12-04 23:15:08 +0300

    Added solution for year 2016 day 9

--- 2016/Day 09 - Explosives in Cyberspace/solution.js
+++ 2016/Day 09 - Explosives in Cyberspace/solution.js
@@ -1,3 +1,30 @@
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	function decompress(input) {
+		let length = 0;
+		while (input.length !== 0) {
+			if (input[0] === "(") {
+				const [descriptor,charCount,repeat] = input.match(/\(([0-9]+)x([0-9]+)\)/);
+				input = input.slice(descriptor.length);
+
+				const stringToRepeat = input.slice(0, +charCount);
+				input = input.slice(+charCount);
+				
+				if (part === 2) {
+					let subLength = decompress(stringToRepeat);
+					length += subLength * repeat;
+				}
+				else {
+					length += stringToRepeat.length * repeat;
+				}
+			}
+			else {
+				const rawData = input.match(/[^(]+/)[0];
+				input = input.slice(rawData.length);
+
+				length += rawData.length;
+			}
+		}
+		return length;
+	}
+	return decompress(input);
 };
