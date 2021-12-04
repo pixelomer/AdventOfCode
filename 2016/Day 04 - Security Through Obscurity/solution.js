00e9e91ae5cf1eb1cb384fa94a9e33ef1d6cae31
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-12-04 20:55:29 +0300

    Added solution for year 2016 day 4

--- 2016/Day 04 - Security Through Obscurity/solution.js
+++ 2016/Day 04 - Security Through Obscurity/solution.js
@@ -1,3 +1,45 @@
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	input = input.split("\n");
+	let sum = 0;
+
+	function decrypt(name, rotation) {
+		const codeForA = "a".charCodeAt(0);
+		const max = "z".charCodeAt(0) - codeForA + 1;
+		const encrypted = name.split("");
+		for (let i=0; i<encrypted.length; i++) {
+			if (encrypted[i] === "-") {
+				encrypted[i] = " ";
+				continue;
+			}
+			const charCode = encrypted[i].charCodeAt(0);
+			const newCharCode = codeForA + ((charCode - codeForA + rotation) % max);
+			encrypted[i] = String.fromCharCode(newCharCode);
+		}
+		const decrypted = encrypted.join("");
+		return decrypted;
+	}
+
+	for (let i=0; i<input.length; i++) {
+		const [ , roomID, sector, checksum ] = input[i].match(/^([^0-9]+?)([0-9]+)\[([a-z]+)\]$/);
+		const roomLetters = roomID.match(/[a-z]/g);
+		const grouped = {};
+		for (const letter of roomLetters) {
+			grouped[letter] = (grouped[letter] ?? 0) + 1;
+		}
+		const expectedChecksum = Object.entries(grouped).sort((a, b) => {
+			return (b[1] - a[1]) || (a[0] > b[0] ? 1 : -1);
+		}).slice(0, 5).map((a) => a[0]).join("");
+		if (expectedChecksum === checksum) {
+			if (part === 2) {
+				const roomName = decrypt(roomID, +sector);
+				if (roomName.includes("north")) {
+					return +sector;
+				}
+			}
+			sum += +sector;
+		}
+	}
+	if (part === 1) {
+		return sum;
+	}
 };
