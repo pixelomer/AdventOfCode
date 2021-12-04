b1dbb4ac571645f3400bafe8df0b01c925ba895f
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-12-04 21:17:04 +0300

    Add solution for year 2016 day 6

--- 2016/Day 06 - Signals and Noise/solution.js
+++ 2016/Day 06 - Signals and Noise/solution.js
@@ -1,3 +1,13 @@
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	input = input.split("\n");
+	let message = "";
+	for (let i=0; i<input[0].length; i++) {
+		const grouped = {};
+		for (let j=0; j<input.length; j++) {
+			grouped[input[j][i]] = (grouped[input[j][i]] ?? 0) + 1;
+		}
+		const sorter = (part === 1) ? ((a,b) => b[1]-a[1]) : ((a,b) => a[1]-b[1]);
+		message += Object.entries(grouped).sort(sorter)[0][0];
+	}
+	return message;
 };
