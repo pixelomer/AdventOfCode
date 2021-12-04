e033a382cbdc636a6e54359a564e4b1f5dbd7971
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-12-04 21:10:40 +0300

    Added solution for year 2016 day 5

--- 2016/Day 05 - How About a Nice Game of Chess?/solution.js
+++ 2016/Day 05 - How About a Nice Game of Chess?/solution.js
@@ -1,3 +1,39 @@
+const crypto = require('crypto');
+
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	let password = [];
+	let found = 0;
+
+	function updateStatus() {
+		const state = [...password];
+		for (let i=0; i<8; i++) {
+			state[i] = state[i] ?? "_";
+		}
+		process.stdout.write(`\rHACKING... [${state.join("")}]`);
+	}
+
+	updateStatus();
+
+	for (let i=0; found < 8; i++) {
+		const data = `${input}${i}`;
+		const hash = crypto.createHash("md5").update(data).digest('hex');
+		if (hash.startsWith("00000")) {
+			if (part === 1) {
+				password[found] = hash[5];
+				found++;
+			}
+			else {
+				const pos = parseInt(hash[5]);
+				if (isNaN(pos) || (pos > 7) || (password[pos] != null)) {
+					continue;
+				}
+				password[pos] = hash[6];
+				found++;
+			}
+			updateStatus();
+		}
+	}
+	
+	process.stdout.write("\n");
+	return password.join("");
 };
