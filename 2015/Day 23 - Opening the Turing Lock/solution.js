455675b3511d705cfd0aa026b4f7e3713dd4f88a
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-12-05 11:52:29 +0300

    Added solution for year 2015 day 23

--- 2015/Day 23 - Opening the Turing Lock/solution.js
+++ 2015/Day 23 - Opening the Turing Lock/solution.js
@@ -1,3 +1,36 @@
-module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+module.exports = (input, part, isTest) => {
+	input = input.split("\n");
+	reg = { 'a':0, 'b':0 };
+	if (part === 2) {
+		reg['a'] = 1;
+	}
+	for (let pc=0; pc<input.length && pc>=0; pc++) {
+		const line = input[pc];
+		const [,instruction,operand1,operand2] = line.match(/^([^ ]+) ([^ ]+)(?:, ([^ ]+))?$/);
+		switch (instruction) {
+			case "hlf":
+				reg[operand1] = Math.floor(reg[operand1] / 2);
+				break;
+			case "tpl":
+				reg[operand1] *= 3;
+				break;
+			case "inc":
+				reg[operand1] += 1;
+				break;
+			case "jmp":
+				pc += +operand1 - 1;
+				break;
+			case "jie":
+				if (reg[operand1] % 2 === 0) {
+					pc += +operand2 - 1;
+				}
+				break;
+			case "jio":
+				if (reg[operand1] === 1) {
+					pc += +operand2 - 1;
+				}
+				break;
+		}
+	}
+	return isTest ? reg['a'] : reg['b'];
 };
