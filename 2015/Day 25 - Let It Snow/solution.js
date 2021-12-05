911332937d2a65cd35d58a70b1c4b7a2b2ac788c
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-12-05 12:03:20 +0300

    Added solution for year 2015 day 25

--- 2015/Day 25 - Let It Snow/solution.js
+++ 2015/Day 25 - Let It Snow/solution.js
@@ -1,3 +1,24 @@
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	if (part === 2) {
+		console.log("There is no part 2.");
+		return;
+	}
+	const [,targetY,targetX] = input.match(/row ([0-9]+), column ([0-9]+)/);
+	let x = 1, y = 1;
+	let code = 20151125;
+	while (true) {
+		if (y === 1) {
+			y = x + 1;
+			x = 1;
+		}
+		else {
+			x++;
+			y--;
+		}
+		code = (code * 252533) % 33554393;
+		if ((targetY == y) && (targetX == x)) {
+			break;
+		}
+	}
+	return code;
 };
