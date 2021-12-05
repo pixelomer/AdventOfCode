fee85129d2d871a3a758ab9be38da7ad56aba9b9
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-12-05 14:34:43 +0300

    Added visualization for year 2021 day 5

--- 2021/Day 05 - Hydrothermal Venture/solution.js
+++ 2021/Day 05 - Hydrothermal Venture/solution.js
@@ -1,6 +1,19 @@
-module.exports = (input, part, isTest) => {
+const Jimp = require('jimp');
+
+/** @returns {Promise<Jimp>} */
+function createImage(width, height) {
+	return new Promise((resolve, reject) => {
+		new Jimp(width, height, (err, image) => {
+			if (err != null) reject(err);
+			else resolve(image);
+		});
+	});
+}
+
+module.exports = async(input, part, isTest) => {
 	input = input.split("\n");
 	const points = {};
+	let maxX = 0, maxY = 0, maxOverlap = 0;
 	for (const line of input) {
 		let [,x1,y1,x2,y2] = line.match(/([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)/);
 		if ((part === 1) && (x1 != x2) && (y1 != y2)) {
@@ -25,13 +38,29 @@ module.exports = (input, part, isTest) => {
 		let prevKey = null;
 		while (true) {
 			x = getX(), y = getY();
+			maxX = Math.max(x, maxX);
+			maxY = Math.max(y, maxY);
 			const key = `${x},${y}`;
 			if (key == prevKey) break;
 			prevKey = key;
 			points[key] = (points[key] ?? 0) + 1;
+			maxOverlap = Math.max(points[key], maxOverlap)
 		}
 	}
+	
+	// Visualization
+	const image = await createImage(maxX+1, maxY+1);
+	for (let x=0; x<maxX; x++) {
+		for (let y=0; y<maxY; y++) {
+			const key = `${x},${y}`;
+			const overlap = points[key] ?? 0;
+			let brightness = Math.floor((overlap / maxOverlap) * 0xFF);
+			const hex = ((brightness << 24) | (brightness << 16) | (brightness << 8) | 0xFF) >>> 0;
+			image.setPixelColor(hex, x, y);
+		}
+	}
+	await image.writeAsync(`part${part}.png`);
+
 	const overlap = Object.values(points).filter((a) => a > 1).length;
-	console.log(overlap);
 	return overlap;
 };
--- solve.js
+++ solve.js
@@ -14,214 +14,218 @@ argv = [
 ] 
 */
 
-let cookie = null;
-if (!fs.existsSync("cookie.txt")) {
-	console.error("WARNING: cookie.txt is missing. Will not submit answers.");
-}
-else {
-	cookie = fs.readFileSync("cookie.txt", { encoding: 'utf-8' });
-}
-
-const consoleLogOrig = console.log;
-const consoleErrorOrig = console.error;
-
-function removeConsoleLog() {
-	console.log = console.error = (...args) => {
-		consoleErrorOrig("[solver]", ...args);
-	};
-}
-
-function restoreConsoleLog() {
-	console.log = consoleLogOrig;
-	console.error = consoleErrorOrig;
-}
-
-const programName = path.basename(process.argv[1]);
-
-if (
-	(process.argv.length !== 5) ||
-	!process.argv[2].match(/^[2-9][0-9]{3}$/g) ||
-	!process.argv[3].match(/^(?:[0-2]?[0-9])|(?:3[0-1])$/) ||
-	!process.argv[4].match(/^[1-2]$/)
-) {
-	console.error(`Usage: ${programName} <year> <day> <part>`);
-	process.exit(1);
-}
-
-const yearString = process.argv[2];
-const dayString = (
-	((process.argv[3].length === 2) ? "" : "0")
-	+ process.argv[3]
-);
-const yearDirectory = fs.opendirSync(yearString);
-
-if (yearDirectory == null) {
-	console.error(`${programName}: invalid year: ${yearString}`);
-	process.exit(1);
-}
-
-let dayDirectory = null;
-while (dayDirectory = yearDirectory.readSync()) {
-	if (dayDirectory.name.match(new RegExp(`^Day ${dayString} \\-`, 'g'))) {
-		break;
+async function main() {
+	let cookie = null;
+	if (!fs.existsSync("cookie.txt")) {
+		console.error("WARNING: cookie.txt is missing. Will not submit answers.");
 	}
-}
-
-if (dayDirectory == null) {
-	console.error(`${programName}: invalid day: ${dayString}`);
-	process.exit(1);
-}
-
-process.chdir(`${yearDirectory.path}/${dayDirectory.name}`);
-yearDirectory.closeSync();
-
-const inputString = fs.readFileSync("input.txt").toString();
-
-if (inputString == null) {
-	console.error(`${programName}: failed to read input.txt`);
-}
-
-const partNumber = parseInt(process.argv[4]);
-let solve = require(`${process.cwd()}/solution.js`);
-
-let result;
-
-try {
-	let testsFailed = false;
-	if (fs.existsSync(`${process.cwd()}/tests`)) {
-		fs.readdirSync(`${process.cwd()}/tests`).forEach((fileName)=>{
-			if (fileName.startsWith(".") || !fileName.endsWith(".txt")) return;
-			let testData = fs.readFileSync(`${process.cwd()}/tests/${fileName}`).toString();
-			const splitTestData = testData.split("\n");
-			testData = splitTestData.slice(1).join("\n");
-			const expectedAnswers = splitTestData[0].split("|");
-			delete splitTestData;
-			function test(part) {
-				if (expectedAnswers.length < part) return;
-				if (expectedAnswers[part-1].length <= 0) return;
-				const start = Date.now();
-				removeConsoleLog();
-				const result = solve(testData, part, true);
-				restoreConsoleLog();
-				const end = Date.now();
-				if (result != expectedAnswers[part-1]) {
-					console.error(`${programName}: test "${fileName}" failed (expected ${expectedAnswers[part-1]}, got ${result})`);
-					testsFailed = true;
-				}
-				else {
-					console.error(`${programName}: test "${fileName}" passed in ${(end - start)/1000.0}s`);
-				}
-			}
-			test(partNumber);
-		});
+	else {
+		cookie = fs.readFileSync("cookie.txt", { encoding: 'utf-8' });
 	}
-	if (testsFailed) {
+
+	const consoleLogOrig = console.log;
+	const consoleErrorOrig = console.error;
+
+	function removeConsoleLog() {
+		console.log = console.error = (...args) => {
+			consoleErrorOrig("[solver]", ...args);
+		};
+	}
+
+	function restoreConsoleLog() {
+		console.log = consoleLogOrig;
+		console.error = consoleErrorOrig;
+	}
+
+	const programName = path.basename(process.argv[1]);
+
+	if (
+		(process.argv.length !== 5) ||
+		!process.argv[2].match(/^[2-9][0-9]{3}$/g) ||
+		!process.argv[3].match(/^(?:[0-2]?[0-9])|(?:3[0-1])$/) ||
+		!process.argv[4].match(/^[1-2]$/)
+	) {
+		console.error(`Usage: ${programName} <year> <day> <part>`);
 		process.exit(1);
 	}
-	removeConsoleLog();
-	result = solve(inputString, partNumber, false);
-	restoreConsoleLog();
-}
-catch (err) {
-	console.error(err.stack);
-	result = null;
-}
 
-if (result != null) {
-	console.log(result);
-	if ((cookie != null) && ((typeof result === 'string') || (typeof result === 'number'))) {
-		process.stdout.write("Terminate program to cancel submission...");
-		setTimeout(() => {
-			console.log("\nSubmitting answer...");
-			const data = `level=${partNumber}&answer=${encodeURIComponent(result)}`;
-			const referer = `https://adventofcode.com/${+yearString}/day/${+dayString}`;
-			const url = `${referer}/answer`;
-			fetch(url, {
-				method: 'POST',
-				headers: {
-					'Cookie': cookie,
-					'Content-Type': 'application/x-www-form-urlencoded',
-					'Referer' : referer
-				},
-				body: data
-			}).then((response) => {
-				if (response.status === 200) {
-					return response.text();
+	const yearString = process.argv[2];
+	const dayString = (
+		((process.argv[3].length === 2) ? "" : "0")
+		+ process.argv[3]
+	);
+	const yearDirectory = fs.opendirSync(yearString);
+
+	if (yearDirectory == null) {
+		console.error(`${programName}: invalid year: ${yearString}`);
+		process.exit(1);
+	}
+
+	let dayDirectory = null;
+	while (dayDirectory = yearDirectory.readSync()) {
+		if (dayDirectory.name.match(new RegExp(`^Day ${dayString} \\-`, 'g'))) {
+			break;
+		}
+	}
+
+	if (dayDirectory == null) {
+		console.error(`${programName}: invalid day: ${dayString}`);
+		process.exit(1);
+	}
+
+	process.chdir(`${yearDirectory.path}/${dayDirectory.name}`);
+	yearDirectory.closeSync();
+
+	const inputString = fs.readFileSync("input.txt").toString();
+
+	if (inputString == null) {
+		console.error(`${programName}: failed to read input.txt`);
+	}
+
+	const partNumber = parseInt(process.argv[4]);
+	let solve = require(`${process.cwd()}/solution.js`);
+
+	let result;
+
+	try {
+		let testsFailed = false;
+		if (fs.existsSync(`${process.cwd()}/tests`)) {
+			for (let fileName of fs.readdirSync(`${process.cwd()}/tests`)) {
+				if (fileName.startsWith(".") || !fileName.endsWith(".txt")) return;
+				let testData = fs.readFileSync(`${process.cwd()}/tests/${fileName}`).toString();
+				const splitTestData = testData.split("\n");
+				testData = splitTestData.slice(1).join("\n");
+				const expectedAnswers = splitTestData[0].split("|");
+				delete splitTestData;
+				async function test(part) {
+					if (expectedAnswers.length < part) return;
+					if (expectedAnswers[part-1].length <= 0) return;
+					const start = Date.now();
+					removeConsoleLog();
+					const result = await solve(testData, part, true);
+					restoreConsoleLog();
+					const end = Date.now();
+					if (result != expectedAnswers[part-1]) {
+						console.error(`${programName}: test "${fileName}" failed (expected ${expectedAnswers[part-1]}, got ${result})`);
+						testsFailed = true;
+					}
+					else {
+						console.error(`${programName}: test "${fileName}" passed in ${(end - start)/1000.0}s`);
+					}
 				}
-				else {
-					console.log("Could not submit answer.");
-					process.exit(1);
-				}
-			}).then((text) => {
-				if (text.includes("You don't seem to be solving the right level.")) {
-					console.log("This part has been solved before.");
-					return fetch(referer, {
-						headers: {
-							'Cookie': cookie
+				await test(partNumber);
+			}
+		}
+		if (testsFailed) {
+			process.exit(1);
+		}
+		removeConsoleLog();
+		result = await solve(inputString, partNumber, false);
+		restoreConsoleLog();
+	}
+	catch (err) {
+		console.error(err.stack);
+		result = null;
+	}
+
+	if (result != null) {
+		console.log(result);
+		if ((cookie != null) && ((typeof result === 'string') || (typeof result === 'number'))) {
+			process.stdout.write("Terminate program to cancel submission...");
+			setTimeout(() => {
+				console.log("\nSubmitting answer...");
+				const data = `level=${partNumber}&answer=${encodeURIComponent(result)}`;
+				const referer = `https://adventofcode.com/${+yearString}/day/${+dayString}`;
+				const url = `${referer}/answer`;
+				fetch(url, {
+					method: 'POST',
+					headers: {
+						'Cookie': cookie,
+						'Content-Type': 'application/x-www-form-urlencoded',
+						'Referer' : referer
+					},
+					body: data
+				}).then((response) => {
+					if (response.status === 200) {
+						return response.text();
+					}
+					else {
+						console.log("Could not submit answer.");
+						process.exit(1);
+					}
+				}).then((text) => {
+					if (text.includes("You don't seem to be solving the right level.")) {
+						console.log("This part has been solved before.");
+						return fetch(referer, {
+							headers: {
+								'Cookie': cookie
+							}
+						});
+					}
+					else if (text.includes("That's the right answer!")) {
+						console.log("Solved!");
+						process.exit(0);
+					}
+					else if (text.includes("You gave an answer too recently")) {
+						console.log("You gave an answer too recently.");
+						const match = text.match(/You have ([0-9ms ]+?) left to wait/);
+						if (match != null) {
+							console.log(`Try again in ${match[1]}.`);
 						}
-					});
-				}
-				else if (text.includes("That's the right answer!")) {
-					console.log("Solved!");
-					process.exit(0);
-				}
-				else if (text.includes("You gave an answer too recently")) {
-					console.log("You gave an answer too recently.");
-					const match = text.match(/You have ([0-9ms ]+?) left to wait/);
+						process.exit(1);
+					}
+					else if (text.includes("your answer is too high")) {
+						console.log("Your answer is too high.");
+					}
+					else if (text.includes("your answer is too low")) {
+						console.log("Your answer is too low.");
+					}
+					else if (text.includes("That's not the right answer")) {
+						console.log("That's not right...");
+					}
+					else {
+						console.log("Couldn't parse response.");
+						process.exit(1);
+					}
+					const match = text.match(/[Pp]lease wait ([0-9]+) minute(s?) before trying again/);
 					if (match != null) {
-						console.log(`Try again in ${match[1]}.`);
+						console.log(`Please wait ${match[1]} minute${match[2]} before trying again.`);
 					}
 					process.exit(1);
-				}
-				else if (text.includes("your answer is too high")) {
-					console.log("Your answer is too high.");
-				}
-				else if (text.includes("your answer is too low")) {
-					console.log("Your answer is too low.");
-				}
-				else if (text.includes("That's not the right answer")) {
-					console.log("That's not right...");
-				}
-				else {
-					console.log("Couldn't parse response.");
-					process.exit(1);
-				}
-				const match = text.match(/[Pp]lease wait ([0-9]+) minute(s?) before trying again/);
-				if (match != null) {
-					console.log(`Please wait ${match[1]} minute${match[2]} before trying again.`);
-				}
-				process.exit(1);
-			}).then((response) => {
-				if (response.status === 200) {
-					return response.text();
-				}
-				else {
-					console.log("Could not fetch correct answer.");
-					process.exit(1);
-				}
-			}).then((text) => {
-				text = text.replace(/\n/mg, "");
-				let match;
-				if (partNumber == 1) {
-					match = text.match(/<p>Your puzzle answer was <code>([^<]+)<\/code>\.<\/p>.*--- Part Two ---/);
-				}
-				else {
-					match = text.match(/--- Part Two ---.*<p>Your puzzle answer was <code>([^<]+)<\/code>\.<\/p>/);
-				}
-				const correctAnswer = match[1];
-				if (correctAnswer == result) {
-					console.log("The answer was correct.");
-					process.exit(0);
-				}
-				else {
-					console.log(`Incorrect answer. The correct answer was ${correctAnswer}.`);
-					process.exit(1);
-				}
-			});
-		}, 1000);
+				}).then((response) => {
+					if (response.status === 200) {
+						return response.text();
+					}
+					else {
+						console.log("Could not fetch correct answer.");
+						process.exit(1);
+					}
+				}).then((text) => {
+					text = text.replace(/\n/mg, "");
+					let match;
+					if (partNumber == 1) {
+						match = text.match(/<p>Your puzzle answer was <code>([^<]+)<\/code>\.<\/p>.*--- Part Two ---/);
+					}
+					else {
+						match = text.match(/--- Part Two ---.*<p>Your puzzle answer was <code>([^<]+)<\/code>\.<\/p>/);
+					}
+					const correctAnswer = match[1];
+					if (correctAnswer == result) {
+						console.log("The answer was correct.");
+						process.exit(0);
+					}
+					else {
+						console.log(`Incorrect answer. The correct answer was ${correctAnswer}.`);
+						process.exit(1);
+					}
+				});
+			}, 1000);
+		}
+	}
+	else {
+		console.error(`${programName}: failed to solve challange`);
+		process.exit(1);
 	}
 }
-else {
-	console.error(`${programName}: failed to solve challange`);
-	process.exit(1);
-}
+
+main();
