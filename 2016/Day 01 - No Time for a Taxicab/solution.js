5b40f70fe0725f05348b8781375641f862cd6caf
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-12-05 16:40:32 +0300

    Added solution for year 2016 day 1

--- 2016/Day 01 - No Time for a Taxicab/solution.js
+++ 2016/Day 01 - No Time for a Taxicab/solution.js
@@ -1,3 +1,33 @@
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	const steps = input.match(/[RL][0-9]+/g);
+	let direction = 0;
+	let x=0, y=0;
+	const getDistance = () => Math.abs(x) + Math.abs(y);
+	const visited = new Set();
+	for (const step of steps) {
+		const change = +step.slice(1);
+		if (step[0] == 'L') direction--;
+		else direction++;
+		if (direction == -1) direction = 3;
+		else direction %= 4;
+		for (let i=0; i<change; i++) {
+			if (part === 2) {
+				visited.add(`${x},${y}`);
+			}
+			switch (direction) {
+				case 0: x++; break;
+				case 1: y++; break;
+				case 2: x--; break;
+				case 3: y--; break;
+			}
+			if (part === 2) {
+				if (visited.has(`${x},${y}`)) {
+					return getDistance();
+				}
+			}
+		}
+	}
+	if (part === 1) {
+		return getDistance();
+	}
 };
