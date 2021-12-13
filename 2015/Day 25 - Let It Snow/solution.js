3f23c91a3c50023d1a704a43bac4f2a633ba2ebd
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-12-13 20:59:43 +0300

    Cleaned up year 2015 day 25 solution

--- 2015/Day 25 - Let It Snow/solution.js
+++ 2015/Day 25 - Let It Snow/solution.js
@@ -6,7 +6,7 @@ module.exports = (input, part) => {
 	const [,targetY,targetX] = input.match(/row ([0-9]+), column ([0-9]+)/);
 	let x = 1, y = 1;
 	let code = 20151125;
-	while (true) {
+	while ((targetY == y) && (targetX == x)) {
 		if (y === 1) {
 			y = x + 1;
 			x = 1;
@@ -16,9 +16,6 @@ module.exports = (input, part) => {
 			y--;
 		}
 		code = (code * 252533) % 33554393;
-		if ((targetY == y) && (targetX == x)) {
-			break;
-		}
 	}
 	return code;
 };
