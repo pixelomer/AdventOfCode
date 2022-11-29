c8a168418c43cf1de3a7f31bbfcb791c8433fd79
pixelomer <37103802+pixelomer@users.noreply.github.com>
2022-11-29 18:55:00 +0300

    Add solution for year 2017 day 6

--- 2017/Day 06 - Memory Reallocation/solution.js
+++ 2017/Day 06 - Memory Reallocation/solution.js
@@ -1,3 +1,33 @@
 module.exports = (input, part) => {
-	// This function is the entry point for the solution.
+	const banks = input.split(/[ \t]/g).filter((a) => a != "").map((a) => +a);
+	function run(secondRun) {
+		let step = 0;
+		const states = new Set();
+		const startState = banks.join(",");
+		while (true) {
+			const state = banks.join(",");
+			if (secondRun && (step !== 0)) {
+				if (state === startState) {
+					break;
+				}
+			}
+			else {
+				if (states.has(state)) {
+					break;
+				}
+				states.add(state);
+			}
+			const i = banks.indexOf([...banks].sort((a,b) => b-a)[0]);
+			for (let j=1; banks[i] > 0; j++) {
+				const k = (i + j) % banks.length;
+				banks[i]--;
+				banks[k]++;
+			}
+			step++;
+		}
+		return step;
+	}
+	let result = run(false);
+	if (part === 1) return result;
+	return run(true);
 };
