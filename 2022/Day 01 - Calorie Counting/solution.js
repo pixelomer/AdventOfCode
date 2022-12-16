8e6fdd8b1ada97002e21bcea421a0ca432755dc2
pixelomer <37103802+pixelomer@users.noreply.github.com>
2022-12-16 20:30:00 +0300

    Shorter solution for year 2022 day 1

--- 2022/Day 01 - Calorie Counting/solution.js
+++ 2022/Day 01 - Calorie Counting/solution.js
@@ -1,15 +1,7 @@
 module.exports = (input, part, isTest) => {
-	input = input.split("\n\n");
-	const elves = [];
-	for (const elf of input) {
-		elves.push(elf.split("\n").map((a) => +a)
-			.reduce((a, b) => a+b, 0));
-	}
-	elves.sort((a,b) => b-a);
-	if (part === 1) {
-		return elves[0];
-	}
-	else {
-		return elves[0] + elves[1] + elves[2];
-	}
+	return input.split("\n\n")
+		.map((a) => a.split("\n").map((a) => +a).reduce((a,b) => a+b))
+		.sort((a,b) => b-a, 0)
+		.slice(0, (part === 1) ? 1 : 3)
+		.reduce((a,b) => a+b, 0);
 };
